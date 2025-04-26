c38e795dbfc7b4ff9d955e11297efc979a584622
Jales Nunes <jales.nunes@hotmail.com>
2025-04-26 15:21:18 -0600

    feat(hero): implement background fireworks video, centralize text and adjust font sizes for responsiveness

---
+++ src/components/hero.tsx
@@ -0,0 +1,31 @@
+export function Hero() {
+    return (
+      <section className="relative w-full h-[60vh] sm:h-[70vh] md:h-[80vh]">
+        {/* Vídeo de fundo */}
+        <video
+          src="public/videos/fireworkvideo.mp4"
+          autoPlay
+          muted
+          loop
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+  
+        {/* Texto sobre o vídeo */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white p-4">
+          <h1 className="font-bebas text-4xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4 mt-8">
+            THE BEST FIREWORKS THAT YOU CAN FIND IN UTAH
+          </h1>
+          <h2 className="font-bebas text-8xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl mb-6">
+            Celebrate with Us!
+          </h2>
+          <a
+            href="#location"
+            className="text-xl sm:text-2xl md:text-3xl font-semibold text-[var(--link-default)] hover:text-[var(--link-hover)]"
+          >
+            Find a Location
+          </a>
+        </div>
+      </section>
+    );
+  }
+  
